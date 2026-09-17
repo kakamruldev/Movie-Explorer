abf82805320e5fc5a74a83e61e529652112ca648
kamrul <kakamrul2000@gmail.com>
2026-09-17 23:13:21 +0600

    feat: add initial HTML structure, CSS styles, and Vite configuration for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 
 export default function App() {
-  return <BrowserRouter><Routes><Route path="/" element={<Home />} />
+  return <HashRouter><Routes><Route path="/" element={<Home />} />
     <Route path="/movies" element={<Movies />} />
     <Route path="*" element={<Home />} />
   </Routes>
-  </BrowserRouter>;
+  </HashRouter>;
 }
