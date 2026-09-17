1dabf4c8a772224aff369a7fd0ce8b5e0f0bca9d
kamrul <kakamrul2000@gmail.com>
2026-09-18 01:55:11 +0600

    feat: update

---
+++ README.md
@@ -0,0 +1,3 @@
+# live link 
+ https://movie-explorer-six-roan.vercel.app/
+ https://movie-dora.netlify.app/
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer id="foot">
-      <Link className="logo" to="/"><span className="mark">R</span> reelora</Link>
+      <Link className="logo" to="/"><span className="mark">M</span> movieDora</Link>
       <p>Made for people who still believe in movie magic.</p>
-      <p>© 2026 Reelora. Powered by TVMaze.</p>
+      <p>© 2026 movieDora. Powered by TVMaze.</p>
     </footer>
   );
 }
